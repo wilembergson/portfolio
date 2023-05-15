94dd63b590c1e794ebe31ad33858d3f7a3b5011a
wilembergson <wilembergson@yahoo.com>
2023-05-15 02:24:57 -0300

    project item container created

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,6 +4,14 @@ export default function Projects() {
             <img className="absolute z-11 w-full  h-full" src="/bg-projects.jpg" alt="" />
             <div className="absolute z-10 w-full h-100 bg-white bg-opacity-10">
                 <h1 className='font-principal mt-16 text-3xl sm:text-5xl text-center mb-10 text-white justify-center'>Projetos</h1>
+                <div className="flex flex-wrap w-full justify-center">
+                    <div className="flex relative flex-col bg-white w-96 h-80 p-5 m-5 items-center rounded-lg">
+                        <img className="absolute z-11 w-11/12 " src="/bg-projects.jpg" alt="" />
+                        <div className="absolute z-10">
+                            <h1>Title 1</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
