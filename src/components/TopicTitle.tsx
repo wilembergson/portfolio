a936034967ea6f82201b8bef35609bb78cb63485
wilembergson <wilembergson@yahoo.com>
2023-05-21 21:37:06 -0300

    discart changes

--- src/components/TopicTitle.tsx
+++ src/components/TopicTitle.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function TopicTitle({ children }: Props) {
     return (
-        <h2 className="text-purple-900 bg-white font-bold p-2 z-12 w-2/3 text-2xl sm:text-4xl mt-20 mb-5" data-aos="fade-left">
+        <h2 className="flex text-purple-900 bg-white font-bold p-2 z-12 w-2/3 text-2xl sm:text-4xl mt-20 mb-5" data-aos="fade-left">
             {children}
         </h2>
     )
--- src/components/content-link.tsx
+++ src/components/content-link.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function ContentLink({ href, children }: Props) {
     return (
-        <span className="font-bold">
+        <span className="flex-wrap font-bold text-justify">
             <Link className="hover:text-yellow-300 duration-300" href={href} target="_blank">
                 {children}
             </Link>
