b4c50016ea7f48714b77486a6b00f321c9d85b2a
wilembergson <wilembergson@yahoo.com>
2023-05-24 12:40:54 -0300

    curriculo adjust

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,19 +1,16 @@
 import Link from 'next/link'
 import { ImDownload2 } from 'react-icons/im'
 
-type Props = {
-    url?: string
-}
-
-export default function MainHeader({ url }: Props) {
+export default function MainHeader() {
     return (
-        <header className="flex fixed bg-white w-full pt-6 pb-6 justify-center z-50 shadow-sm" data-aos="fade-down">
+        <header className="flex fixed bg-white w-full py-5 justify-center z-50 shadow-sm" data-aos="fade-down">
             <div className="flex flex-row justify-between w-11/12 sm:w-2/3">
                 <h1 className="flex flex-row items-center font-black text-center text-purple-900 font-principal text-1xl sm:text-2xl w-max">
                     Wilembergson Alfredo
                 </h1>
                 <Link href='/curriculo.pdf' target='blank' download>
-                    <button className="flex flex-row items-center text-center text-1xl sm:text-2xl text-white p-3 cursor-pointer bg-purple-800 hover:bg-purple-600 duration-700 rounded-full font-principal">
+                    <button className="flex flex-row items-center justify-center text-center text-1xl sm:text-2xl text-white px-3 py-2
+                     cursor-pointer bg-purple-800 hover:bg-purple-600 duration-700 rounded-full font-principal">
                         <ImDownload2 className='mr-2' />curriculo
                     </button>
                 </Link>
