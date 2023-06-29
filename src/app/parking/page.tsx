ebfda264a89a12907abdf3173f5b2ed6ce1719e7
wilembergson <wilembergson@yahoo.com>
2023-06-29 01:21:42 -0300

    Parking project repositories links

--- src/app/parking/page.tsx
+++ src/app/parking/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import TopicTitle from "@/components/TopicTitle";
 import Content from "@/components/content";
+import ContentLink from "@/components/content-link";
 import MainTitle from "@/components/main-title";
 import ProjectPage from "@/components/project-page";
 
@@ -12,11 +13,10 @@ export default function Greenmarket() {
             <TopicTitle>Descrição</TopicTitle>
             <Content color="text-white">
                 Sistema para controle de fluxo de veículos em estacionamento. É possível obter quais vagas
-                estão ocupadas ou disponíveis, cadastrar novos clientes, registrar quais veículos entraram 
+                estão ocupadas ou disponíveis, cadastrar novos clientes, registrar quais veículos entraram
                 ou sairam, conferir quando o cliente irá pagar por hora, além de obter relatórios sobre faturamentos.
             </Content>
             <TopicTitle>Tecnologias utilizadas</TopicTitle>
-
             <Content color="text-white">
                 <ul className="list-disc space-y-5 pl-6">
                     <li>
@@ -36,6 +36,21 @@ export default function Greenmarket() {
                     </li>
                     <li>
                         Docker
+                    </li>
+                </ul>
+            </Content>
+            <TopicTitle>Reporitórios</TopicTitle>
+            <Content color="text-white">
+                <ul className="list-disc space-y-5 pl-6">
+                    <li>
+                        Frontend: <ContentLink href='https://github.com/wilembergson/dev-parking-frontend'>
+                            https://github.com/wilembergson/dev-parking-frontend
+                        </ContentLink>
+                    </li>
+                    <li>
+                        Backend:  <ContentLink href='https://github.com/wilembergson/dev-parking-backend'>
+                            https://github.com/wilembergson/dev-parking-backend
+                        </ContentLink>
                     </li>
                 </ul>
             </Content>
